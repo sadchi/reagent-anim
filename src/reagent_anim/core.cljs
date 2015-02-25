b142a650d296a5fc8c8006842a7ba11da209e40c
Alexey Huba <sadchi@gmail.com>
2015-02-25 20:19:20 +0300

    ref

--- src/reagent_anim/core.cljs
+++ src/reagent_anim/core.cljs
@@ -3,14 +3,20 @@
 
 
 (def class (r/atom "cl1"))
+(def temp (r/atom "Just a text blblblblbllblbb"))
 
 (defn swap-class [a-value]
   (reset! a-value (condp = @a-value
                     "cl1" "cl2"
                     "cl2" "cl1")))
 
+(defn sub-elem [a-val]
+  (fn []
+    (.log js/console "sub rendered")
+    [:div @a-val]))
+
 (defn elem [a-class]
   (fn []
-    [:div {:class @a-class :on-click #(swap-class a-class)} "TEST STRING test string"]))
+    [:div {:class @a-class :on-click #(swap-class a-class)} [sub-elem temp]]))
 
 (r/render-component [elem class] (.getElementById js/document "test"))
